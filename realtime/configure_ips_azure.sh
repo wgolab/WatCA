fc776ca51ebd42f3852fc8e4fe9b530ce2c7f644
Wojciech Golab <wgolab@uwaterloo.ca>
2016-08-26 10:26:30 -0400

    Updated VM configuration scripts

--- realtime/configure_ips_azure.sh
+++ realtime/configure_ips_azure.sh
@@ -3,7 +3,6 @@
 USR=ubuntu
 rm -f servers_public
 rm -f servers_public_private
-rm -f azure_vms
 
 echo
 echo Reading list of resource groups from azure_resource_groups
@@ -15,9 +14,9 @@ do
   azure resource list | grep virtualMachine | tr -s ' ' | cut -d ' ' -f 3 > azure_vms_tmp
   for VM in `cat azure_vms_tmp`
   do
-      echo $RESOURCE_GROUP $VM >> azure_vms 
       PUBIP=`azure resource show $RESOURCE_GROUP ${VM}-ip Microsoft.Network/publicIPAddresses -o "2015-06-15" | grep ipAddress | tr -s ' ' | cut -d ' ' -f 4`
       echo $PUBIP >> servers_public
+      echo "    logging in as $USR@$PUBIP"
       PRVIP=`ssh $USR@$PUBIP "ifconfig | grep 'inet addr' | grep Bcast | cut -d':' -f2 | cut -d' ' -f1 "`
       echo "    found VM $VM with IP $PUBIP/$PRVIP"
       echo $PUBIP $PRVIP >> servers_public_private
@@ -25,5 +24,7 @@ do
 done
 
 echo
-echo Wrote public and private IP addresses to servers_public_private:
+echo Wrote public and private IP addresses to servers_public and servers_public_private:
 cat servers_public_private
+
+rm -f azure_vms_tmp
---
+++ realtime/configure_ips_ec2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USR=ubuntu
+rm -f servers_public
+rm -f servers_public_private
+
+echo
+echo Reading list of EC2 regions for ec2_regions
+echo
+
+for REGION in ` cat ec2_regions`
+do
+  echo Region: $REGION
+  export EC2_URL=https://ec2.${REGION}.amazonaws.com
+  ec2-describe-instances | grep 'running' | cut -d$'\t' -f 17 > servers_public_tmp
+  echo Found `cat servers_public_tmp`
+  for PUBIP in `cat servers_public_tmp`
+  do
+      echo $PUBIP >> servers_public
+      echo "    logging in as $USR@$PUBIP"
+      PRVIP=`ssh $USR@$PUBIP "ifconfig | grep 'inet addr' | grep Bcast | cut -d':' -f2 | cut -d' ' -f1 "`
+      echo "    found VM with IP $PUBIP/$PRVIP"
+      echo $PUBIP $PRVIP >> servers_public_private
+  done
+done
+
+echo
+echo Wrote public and private IP addresses to servers_public and servers_public_private:
+cat servers_public_private
+
+rm -f servers_public_tmp
