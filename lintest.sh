2bbded880f864623434bc3c7d2642a805d0b2e36
Wojciech Golab <wgolab@uwaterloo.ca>
2017-02-08 09:23:24 -0500

    Cleanup.

--- lintest.sh
+++ lintest.sh
@@ -2,14 +2,18 @@
 
 export CLASSPATH=".:build:lib/*"
 
-echo Running
-java -cp ".:target/WatCA-1.0-SNAPSHOT.jar" ca.uwaterloo.watca.LinearizabilityTest execution.log scores.txt
+INPUT_DIR=lintest_input
+OUTPUT_DIR=lintest_output
+mkdir $INPUT_DIR &> /dev/null
+mkdir $OUTPUT_DIR &> /dev/null
+rm -f $INPUT_DIR/*
+rm -f $OUTPUT_DIR/*
+cp execution.log $INPUT_DIR/
 
+echo Running
+java -cp ".:target/WatCA-1.0-SNAPSHOT.jar" ca.uwaterloo.watca.LinearizabilityTest $INPUT_DIR $OUTPUT_DIR
+cp $OUTPUT_DIR/scores.* scores.txt
 echo Done
-#echo Top of scores.txt file:
-#head scores.txt
-#echo Set of distinct scores observed in scores.txt:
-#cat scores.txt | rev | cut -d' ' -f1 | sort -u
 echo Score 0: `cat scores.txt | grep 'Score = 0' | wc -l` occurrences
 echo Score 1: `cat scores.txt | grep 'Score = 1' | wc -l` occurrences
 echo Score 2: `cat scores.txt | grep 'Score = 2' | wc -l` occurrences
