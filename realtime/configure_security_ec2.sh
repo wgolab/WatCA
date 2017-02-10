1faf3a99d595434868ddccbb582f62c21e7e653a
Wojciech Golab <wgolab@uwaterloo.ca>
2017-02-09 21:03:52 -0500

    Fixed bug in multi-region case.

--- realtime/configure_security_ec2.sh
+++ realtime/configure_security_ec2.sh
@@ -17,12 +17,17 @@ echo Reading list of EC2 regions for ec2_regions
 echo
 
 echo Creating security policies:
+rm -f servers_public_tmp
 for REGION in `cat ec2_regions`
 do
     echo Region: $REGION
     export EC2_URL=https://ec2.${REGION}.amazonaws.com
-    ec2-describe-instances | grep 'running' | cut -d$'\t' -f 17 > servers_public_tmp
-    echo Found `cat servers_public_tmp`
+    ec2-describe-instances | grep 'running' | cut -d$'\t' -f 17 >> servers_public_tmp
+done
+echo Found `cat servers_public_tmp`
+
+for REGION in `cat ec2_regions`
+do
     aws ec2 --region $REGION authorize-security-group-ingress --group-name default --protocol all --cidr ${THISHOSTIP}/32
     for PUBIP in `cat servers_public_tmp`
     do
