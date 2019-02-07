b62865d155c21b5f8c1d627b6cc4e8de6fe0f256
Wojciech Golab <wgolab@uwaterloo.ca>
2019-02-07 12:58:03 -0500

    Fixed build.sh error.

--- realtime/run_watca.sh
+++ realtime/run_watca.sh
@@ -6,8 +6,7 @@ cd ${DIR}
 source settings.sh
 
 cd $DIR/..
-bash build.sh
-
+mvn install
 
 cd $DIR
 # this line is only needed in the first time of running to send files.
@@ -21,4 +20,5 @@ bash sync_files.sh init
 rm scores.txt
 export CLASSPATH=../target/classes/
 # TODO Hua args "localhost 12347 javaControl" is meaning less, for historical reason, will be cleaned up
-java ca.uwaterloo.watca.RealtimeMain $ServerLogPort $WebPort localhost 12347 javaControl
+rm watca*.log
+java ca.uwaterloo.watca.RealtimeMain $ServerLogPort $WebPort localhost 12357 javaControl saveLogs
